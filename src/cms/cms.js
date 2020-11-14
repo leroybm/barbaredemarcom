79e1de7ab8e8f408dcc980aea8225215b5b1f390
Leroy <lelo.drax@gmail.com>
2020-11-14 17:52:19 -0300

    Therapy Type - Adds preview to CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,8 +4,10 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import TerapiaIndividualPreview from './preview-templates/TerapiaIndividualPreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('terapia-individual', TerapiaIndividualPreview)
