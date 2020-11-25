92f7f8645ec03847583b579371d4ec2f53809c3b
Leroy <lelo.drax@gmail.com>
2020-11-25 19:35:20 -0300

    Adds a nice fade for social elements

--- src/components/SocialNetworks.js
+++ src/components/SocialNetworks.js
@@ -2,11 +2,38 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagramSquare, faWhatsappSquare, faMedium } from '@fortawesome/free-brands-svg-icons'
+import { throttle } from 'lodash';
+
+const FOOTER_HEIGHT = 416;
 
 const SocialNetworks = class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hidden: false,
+    }
+  }
+
+  scrollHandler() {
+    this.setState({
+      hidden: (window.pageYOffset + window.innerHeight) >= (window.document.body.offsetHeight - FOOTER_HEIGHT)
+    })
+  }
+
+  componentDidMount() {
+    this.throttledScrollHandle = throttle(this.scrollHandler.bind(this), (1000 / 60)); // The human eye can't see past 30 fps
+    window.addEventListener('scroll', this.throttledScrollHandle);
+  }
+
+  componentWillUnmount() {
+    if (this.throttledScrollHandle) {
+      window.removeEventListener(this.throttledScrollHandle);
+    }
+  }
+
   render() {
     return (
-      <div className="c-social-networks">
+      <div className={`c-social-networks ${this.state.hidden ? 'hidden' : ''}`}>
         <Link to="https://instagram.com" target="_blank">
           <FontAwesomeIcon icon={faInstagramSquare}/>
         </Link>
